4d1518b5cdb1d6ef8951b1801909881b6049be8c
Scott Littlewood <scottlittlewood@gmail.com>
2022-01-28 09:26:04 +0000

    feat: :sparkles: use pointer for button on hover

--- src/routes/todos/index.svelte
+++ src/routes/todos/index.svelte
@@ -197,6 +197,7 @@
 	.delete {
 		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.5 5V22H19.5V5H4.5Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M10 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M14 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M2 5H22' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M8 5L9.6445 2H14.3885L16 5H8Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3C/svg%3E%0A");
 		opacity: 0.2;
+		cursor: pointer;
 	}
 
 	.delete:hover,
